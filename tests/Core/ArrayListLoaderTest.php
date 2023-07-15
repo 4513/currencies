4f0f6da9839ed270385643d1e317351cfdb5af6b
MiBo <michal.boris@feo.cz>
2023-07-15 13:09:10 +0200

    feat(Loader): Array List Loader

---
+++ tests/Core/ArrayListLoaderTest.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MiBo\Currencies\Tests;
+
+use MiBo\Currencies\ISO\ISOArrayListLoader;
+use MiBo\Currencies\ISO\ISOListLoader;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ArrayListLoaderTest
+ *
+ * @package MiBo\Currencies\Tests
+ *
+ * @author Michal Boris <michal.boris27@gmail.com>
+ *
+ * @since 1.2
+ *
+ * @no-named-arguments Parameter names are not covered by the backward compatibility promise.
+ *
+ * @coversDefaultClass \MiBo\Currencies\ISO\ISOArrayListLoader
+ */
+class ArrayListLoaderTest extends TestCase
+{
+    /**
+     * @small
+     *
+     * @covers ::loop
+     *
+     * @return void
+     */
+    public function test(): void
+    {
+        $loader = new ISOArrayListLoader();
+
+        foreach ($loader->loop() as $currency) {
+            $this->assertIsObject($currency);
+            $this->assertSame("AFGHANISTAN", $currency->CtryNm);
+            $this->assertSame("Afghani", $currency->CcyNm);
+            $this->assertSame("AFN", $currency->Ccy);
+            $this->assertSame("971", $currency->CcyNbr);
+            $this->assertSame("2", $currency->CcyMnrUnts);
+
+            break;
+        }
+    }
+
+    /**
+     * @large
+     *
+     * @coversNothing
+     * @doesNotPerformAssertions
+     *
+     * @return void
+     */
+    public function create(): void
+    {
+        return;
+
+        if (file_exists(__DIR__ . "/tmp.php")) {
+            unlink(__DIR__ . "/tmp.php");
+        }
+
+        touch(__DIR__ . "/tmp.php");
+
+        $a      = "<?php\n\nreturn [\n    ";
+        $loader = new ISOListLoader(ISOListLoader::SOURCE_WEB);
+
+        foreach ($loader->loop() as $currency) {
+            $currency->CtryNm = addslashes((string) $currency->CtryNm);
+            $currency->CcyNm  = addslashes((string) $currency->CcyNm);
+            $a               .= "[\n        'CtryNm'     => '{$currency->CtryNm}',\n";
+            $a               .= "        'CcyNm'      => '{$currency->CcyNm}',\n";
+            $a               .= "        'Ccy'        => '{$currency->Ccy}',\n";
+            $a               .= "        'CcyNbr'     => '{$currency->CcyNbr}',\n";
+            $a               .= "        'CcyMnrUnts' => '{$currency->CcyMnrUnts}',\n    ],\n    ";
+        }
+
+        $a .= "];\n";
+
+        file_put_contents(__DIR__ . "/tmp.php", $a);
+    }
+}
